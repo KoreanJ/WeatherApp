af3094ab23ae11731614ae067e974ffad0633e51
Nicholas Smith <nicholas.smith.126@us.af.mil>
2025-11-28 21:23:45 -0800

    Add project files.

---
+++ ViewModels/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModels
+{
+    public class Home : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WeatherApp.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
