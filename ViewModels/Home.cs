5c49ee82235f72c311c722d803892c802377f294
Nicholas Smith <nicholas.smith.126@us.af.mil>
2025-11-29 00:12:51 -0800

    Following sample project on Avalonia UI GitHub. Created a simple ViewModel to demonstrate real-time data updates

--- ViewModels/Home.cs
+++ ViewModels/Home.cs
@@ -4,11 +4,53 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace WeatherApp.ViewModels
 {
     public class Home : INotifyPropertyChanged
     {
+        private string? _Name;
+
+        private string? _Greeting;
+
+        public string? Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(nameof(Greeting));
+                }
+            }
+        }
+
+        public string? Greeting
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Name))
+                {
+                    return "Hello Default User!";
+                }
+                else
+                {
+                    return $"Hello {Name}!";
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
